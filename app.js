0b0395b07f84ccc6600e833875e17cb42821ee25
Kaitlin734 <86024766+Kaitlin734@users.noreply.github.com>
2022-06-15 10:03:59 +1200

    Landing page

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const menu = document.querySelector('#menu');
 const menuLinks = document.querySelector('.navbarMenu');
 
+// Drop down menu bar that forms an x when activated
 const Menu = () => {
 menu.classList.toggle('is-active');
 menuLinks.classList.toggle('active');
